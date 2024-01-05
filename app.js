443964cbada6bc9b43bf453b4b889784c2101e12
Olha Teplova <olitieplove@gmail.com>
2024-01-05 21:48:14 +0200

    refactor(basic-3.4): replace tag selectors with class ones, leave some tag selectors to reset styles to default

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector("input[type=text]");
-  var label=listItem.querySelector("label");
+  var editInput=listItem.querySelector(".task-text");
+  var label=listItem.querySelector(".task-label");
   var editBtn=listItem.querySelector(".edit-task");
   var containsClass=listItem.classList.contains("edit-mode");
   //If class of the parent is .editmode
@@ -155,9 +155,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   //select ListItems children
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit-task");
-  var deleteButton=taskListItem.querySelector("button.delete-task");
+  var checkBox=taskListItem.querySelector(".task-checkbox");
+  var editButton=taskListItem.querySelector(".edit-task");
+  var deleteButton=taskListItem.querySelector(".delete-task");
 
 
   //Bind editTask to edit button.
